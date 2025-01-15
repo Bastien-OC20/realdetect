a684eec2d92a010202e60954c58c2174f1d0c6f1
Sebastien <bastien.rapuzzi@gmail.com>
2025-01-15 13:54:23 +0100

    Rendering status OK

--- components/interface.py
+++ components/interface.py
@@ -3,7 +3,9 @@ import taipy as tp
 def start_gui():
     """Lance l'interface Taipy pour afficher les résultats."""
     page = """
-    <h1>Webcam Object Detection and Captioning</h1>
-    <img src="http://127.0.0.1:5000/video_feed" alt="Current Webcam Feed"/>
+    <div style="text-align: center;">
+        <h1>Webcam Object Detection and Captioning</h1>
+        <img src="http://127.0.0.1:5000/video_feed" alt="Current Webcam Feed" style="width: 50%;"/>
+    </div>
     """
     tp.Gui(page=page).run()
--- components/webcam.py
+++ components/webcam.py
@@ -3,8 +3,12 @@ from components.processing import process_frame
 from components.models import initialize_object_detection, initialize_image_captioning
 from PIL import Image
 
+current_caption = ""
+current_labels = []
+
 def generate_video_stream():
     """Génère un flux vidéo MJPEG pour Flask."""
+    global current_caption, current_labels
     obj_processor, obj_model, device = initialize_object_detection()
     caption_pipeline = initialize_image_captioning()
 
@@ -23,8 +27,9 @@ def generate_video_stream():
         frame = cv2.flip(frame, 1)
 
         # Processus de détection et génération de description
-        frame, image = process_frame(frame, obj_processor, obj_model, device)
-        description = caption_pipeline(image)[0]['generated_text']
+        frame, image, labels = process_frame(frame, obj_processor, obj_model, device)
+        current_caption = caption_pipeline(image)[0]['generated_text']
+        current_labels = labels
 
         # Encode l'image en JPEG
         ret, jpeg = cv2.imencode('.jpg', frame)
@@ -39,3 +44,11 @@ def generate_video_stream():
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
 
     cap.release()
+
+def get_current_caption():
+    """Retourne la légende actuelle."""
+    return current_caption
+
+def get_current_labels():
+    """Retourne les étiquettes actuelles."""
+    return current_labels
